a6825241213525e8928c6dab6445fed68fbbf51f
Kyle Geske <stungeye@gmail.com>
2020-02-01 07:45:42 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <QuizGame/>
+  </div>
+</template>
+
+<script>
+import QuizGame from "./components/QuizGame";
+
+export default {
+  name: "App",
+  components: {
+    QuizGame
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 1em;
+}
+</style>
---
+++ src/components/QuizGame.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="game">
+    <div class="question">
+      <div class="operand">56</div>
+      <div class="operator">+</div>
+      <div class="operand">34</div>
+    </div>
+    <keypad :onInput="onInput" :onSubmit="onSubmit" :onReset="onReset" :show="showKeypad"/>
+  </div>
+</template>
+
+<script>
+import keypad from "@/components/KeyPad.vue";
+
+export default {
+  name: "QuizGame",
+  components: {
+    keypad
+  },
+  data: () => ({
+    number: "",
+    maxLength: 6,
+    showKeypad: true
+  }),
+  methods: {
+    onInput(key) {
+      this.number = (this.number + key).slice(0, this.maxLength);
+    },
+    onSubmit() {
+      alert("Sumitted: " + this.number);
+    },
+    onReset() {
+      this.number = "";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.game {
+  color: red;
+  font-size: 16px;
+}
+.question {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(6, 100px);
+}
+</style>
